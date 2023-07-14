0c9381ab23ded683de4b9720f1536651f0e67f84
Samisweet018 <okolosamuel1809@gmail.com>
2023-07-14 09:20:47 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  main - A programme that uses the puts function
+ *
+ *  Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
