2b27a8d4b02e1361996b40f64a6c39d3904cef97
Samisweet018 <okolosamuel1809@gmail.com>
2023-07-14 19:36:09 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: A program that prints a line using printf function
+ * main - Entry point (A program with a line using printf function)
  *
  * Return: Always 0 (Success)
  */
