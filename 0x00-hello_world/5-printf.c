82b281fc137fbdc1649c048ea87db2c072d043ff
Samisweet018 <okolosamuel1809@gmail.com>
2023-07-14 18:40:56 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programme that uses function printf
+ *
+ * Rerun: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
