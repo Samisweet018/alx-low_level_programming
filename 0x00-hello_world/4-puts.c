8deb5cf9348598909d1d5c5813b52da9faec819c
Samisweet018 <okolosamuel1809@gmail.com>
2023-07-15 17:42:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using puts function
- *
- * Return: Always 0 (Success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Description: A C program that prints the sizes of various computer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n,"(unasigned long)sizeof(a));
+	printf("Size of a int: %lu byte(s)\n,"(unasigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n,"(unasigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n,"(unasigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n,"(unasigned long)sizeof(f));
+	return (0);
+}
