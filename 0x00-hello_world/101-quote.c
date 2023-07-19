6464ec1e633f2baf73d1072c7ed7c3c6ccbdac8a
Samisweet018 <okolosamuel1809@gmail.com>
2023-07-19 06:52:30 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpor, 2015- 10- 19\n", 59);
+	return (1);
+}
