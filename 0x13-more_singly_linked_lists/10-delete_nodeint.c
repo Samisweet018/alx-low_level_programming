0ef983e9799721ec0de114ad007d670342374078
samisweet018 <okolosamuel1809@gmail.com>
2023-08-29 20:38:13 -0700

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,12 +2,12 @@
 
 /**
  * delete_nodeint_at_index - deletes a node in a linked list a certain index
- * @head: a pointer to the first element in the list
- * @index: that is index of the node to delete
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
  *
  * Return: 1 (Success), or -1 (Fail)
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
 	listint_t *current = NULL;
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - that frees a linked list
- * @h:  A pointer to the first node in the linked list
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
  *
- * Return: num of elements in the freed list
+ * Return: number of elements in the freed list
  */
-size_t free_listint_safe(listint_t **h);
+size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
 	int diff;
