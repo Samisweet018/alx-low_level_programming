1cc95c03ea8e566d9203e664650f44d7909ceefa
Samuel Okolo <okolosamuel1809@gmail.com>
2023-11-21 08:23:49 -0800

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,6 @@
  * @c: char to assign
  * Description: create array of size size and assign char c
  * Return: pointer to array, NULL if fail
- *
  */
 char *create_array(unsigned int size, char c)
 {
